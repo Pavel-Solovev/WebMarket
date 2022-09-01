7eceab4ae3d28efebfafa051e78e427f1fe8d210
Pavel Solovev <pavelsol1990@gmail.com>
2022-09-01 21:30:42 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+
+
+  document.getElementById('root'));
+
 );
 
 // If you want to start measuring performance in your app, pass a function
